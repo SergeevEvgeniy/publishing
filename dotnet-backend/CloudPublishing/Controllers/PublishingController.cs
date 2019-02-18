5e162abd20bd08541365e0c2d69b121ac52a04b6
Nikita Podobed <Nikita.Podobed@artezio.com>
2019-02-18 18:08:13 +0300

    Добавлен контроллер PublishingController и представление List для него; добавлены модели Publishing и Topic, файл контекста PublishingContext; интерфейс IPublishingRepository и его реализация PublishingRepository

---
+++ dotnet-backend/CloudPublishing/Controllers/PublishingController.cs
@@ -0,0 +1,23 @@
+﻿using CloudPublishing.Models.Publishings.Entities;
+using CloudPublishing.Models.Publishings.Repositories;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace CloudPublishing.Controllers
+{
+    public class PublishingController : Controller
+    {
+        IPublishingRepository repository;
+
+        public PublishingController()
+        {
+            //TODO: Внедрить зависимости
+            repository = new PublishingRepository();
+        }
+        // GET: Publishing
+        public ActionResult List()
+        {
+            return View(repository.Publishings);
+        }
+    }
+}
