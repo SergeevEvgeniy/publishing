199108ab80ca2d6834881073da4835f905ba1cdf
Nikita Podobed <Nikita.Podobed@artezio.com>
2019-02-25 17:21:08 +0300

    Все файлы, связанные с Журналами, удалены из Web-слоя. В Data-Layer добавлены сущности Publishing и Topic, репозиторий PublishingRepository

---
+++ dotnet-backend/CloudPublishing.Data/Entities/Publishing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudPublishing.Data.Entities
+{
+    public class Publishing
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Subjects { get; set; }
+        public IEnumerable<Topic> Topics { get; set; }
+    }
+}
---
+++ dotnet-backend/CloudPublishing.Data/Repositories/PublishingRepository.cs
@@ -0,0 +1,46 @@
+﻿using CloudPublishing.Data.EF;
+using CloudPublishing.Data.Entities;
+using CloudPublishing.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudPublishing.Data.Repositories
+{
+    public class PublishingRepository : IRepository<Publishing>
+    {
+        private readonly CloudPublishingContext context;
+
+        public PublishingRepository(CloudPublishingContext context)
+        {
+            this.context = context;
+        }
+
+        public Publishing Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Publishing> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Publishing item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Publishing item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
