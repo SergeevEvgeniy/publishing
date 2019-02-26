6b0df6b09d6d6e9eff28da46bc2e216db2fd6383
Vadim Putsick <trewyut@mail,ru>
2019-02-26 22:28:12 +0300

    Добавлен фиктивный сервер для проверки работы поиска журналистов

---
+++ js-frontend/mock-server/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var port = 3010;
+app
+    .use(cors())
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({
+        extended: true
+    }))
+    .get('/', (req, res) => {
+        res.send("hello");
+    })
+    .get('/api/publishings', (req, res) => {
+        var publishings = require('./data/publishings');
+        res.json(publishings);
+    })
+    .get('/api/topics', (req, res) => {
+        var publishings = require('./data/topics');
+        res.json(publishings);
+    })
+    .get('/api/issues', function (req, res) {
+        var issues = require('./data/issues');
+        var id = + req.query.id;
+        var publishingIssues = issues.find((issues) => issues.publishingId === id);
+        res.json(publishingIssues);
+    })
+    .post('/api/journalists', function (req, res) {
+        var journalists = require('./data/journalists');
+        var body = req.body;
+        res.json(journalists);
+    })
+    .listen(port, () => {
+        console.log('App work on ' + port + ' locallhost');
+    });
