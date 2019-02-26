0195cc0f8919974b8811e802e0556291188f9890
Aleksandr Svetnizki <aleksandr.svetnizki@artezio.com>
2019-02-26 16:46:18 +0300

    Изменения в Business-уровне В интерфейсе IReviewService добавлены необходимые методы. Добавлен класс, реализующий интерфейс IReviewService ReviewService. В модуле autofac BusinessModule зарегистрирована зависимость ReviewService и IReviewService.

--- dotnet-backend/CloudPublishing.Business/Services/Interfaces/IReviewService.cs
+++ dotnet-backend/CloudPublishing.Business/Services/Interfaces/IReviewService.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using CloudPublishing.Business.DTO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CloudPublishing.Business.Services.Interfaces
 {
     interface IReviewService
     {
+        IEnumerable<PublishingDTO> GetPublishingList();
+
+        IEnumerable<TopicDTO> GetTopicList(int publishingId);
+
+        IEnumerable<EmployeeDTO> GetAuthorList(int publishingId, int topicId);
+
+        IEnumerable<ArticleDTO> GetArticleList(int publishingId, int topicId, int authorId);
+
+        ReviewDTO GetReview(int articleId, int authorId);
+
+        void UpdateReview(ReviewDTO review);
+
+        void DeleteReview(int articleId, int authorId);
     }
 }
---
+++ dotnet-backend/CloudPublishing.Business/Services/ReviewService.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using CloudPublishing.Business.DTO;
+using CloudPublishing.Data.Entities;
+using CloudPublishing.Data.Interfaces;
+using System.Collections.Generic;
+
+namespace CloudPublishing.Business.Services
+{
+    class ReviewService
+    {
+        private IUnitOfWork db;
+
+        public ReviewService(IUnitOfWork db)
+        {
+            this.db = db;
+        }
+
+        IEnumerable<PublishingDTO> GetPublishingList()
+        {
+            // Заглушка. Будет реализован запрос
+            return null;
+        }
+
+        IEnumerable<TopicDTO> GetTopicList(int publishingId)
+        {
+            // Заглушка. Будет реализован запрос
+            return null;
+        }
+
+        IEnumerable<EmployeeDTO> GetAuthorList(int publishingId, int topicId)
+        {
+            // Заглушка. Будет реализован запрос
+            return null;
+        }
+
+        IEnumerable<ArticleDTO> GetArticleList(int publishingId, int topicId, int authorId)
+        {
+            // Заглушка. Будет реализован запрос
+            return null;
+        }
+
+        ReviewDTO GetReview(int articleId, int authorId)
+        {
+            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Review, ReviewDTO>()).CreateMapper();
+            return mapper.Map<Review, ReviewDTO>(db.Reviews.Get(articleId, authorId));
+        }
+
+        void UpdateReview(ReviewDTO review)
+        {
+            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<ReviewDTO, Review>()).CreateMapper();
+            db.Reviews.Update(mapper.Map<ReviewDTO, Review>(review));
+        }
+
+        void DeleteReview(int articleId, int authorId)
+        {
+            db.Reviews.Delete(articleId, authorId);
+        }
+    }
+}
