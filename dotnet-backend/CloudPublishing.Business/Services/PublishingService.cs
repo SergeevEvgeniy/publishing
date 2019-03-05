6ddb69f7169f4eeafbf9e5013761f38de3a1e283
Nikita Podobed <Nikita.Podobed@artezio.com>
2019-03-05 18:06:27 +0300

    Добавлена функциональность в PublishingService

--- dotnet-backend/CloudPublishing.Business/Services/PublishingService.cs
+++ dotnet-backend/CloudPublishing.Business/Services/PublishingService.cs
@@ -1,32 +1,64 @@
-﻿using CloudPublishing.Business.DTO;
-using CloudPublishing.Business.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using CloudPublishing.Data.Interfaces;
-using System.Linq;
+﻿using AutoMapper;
 using CloudPublishing.Business.Converters;
-using System.Text;
-using System.Threading.Tasks;
+using CloudPublishing.Business.DTO;
+using CloudPublishing.Business.Services.Interfaces;
+using CloudPublishing.Business.Util;
+using CloudPublishing.Data.Entities;
+using CloudPublishing.Data.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CloudPublishing.Business.Services
 {
     public class PublishingService : IPublishingService
     {
         private readonly IUnitOfWork unitOfWork;
+        private IMapper mapper;
 
         public PublishingService(IUnitOfWork unitOfWork)
         {
             this.unitOfWork = unitOfWork;
+            mapper = new MapperConfiguration(cfg => cfg.AddProfile(new PublishingBusinessMapProfile())).CreateMapper();
         }
 
         public IEnumerable<PublishingDTO> GetAllPublishings()
         {
-            return unitOfWork.Publishings.GetAll().Select(x => x.ToDTO());
+            return mapper.Map<IEnumerable<Publishing>, IEnumerable<PublishingDTO>>(unitOfWork.Publishings.GetAll());
+        }
+
+        public PublishingDTO GetPublishing(int id)
+        {
+            PublishingDTO mappedPublishing = null;
+            Publishing publishing = unitOfWork.Publishings.Get(id);
+
+            if (publishing != null)
+            { 
+                mappedPublishing = mapper.Map<Publishing, PublishingDTO>(publishing);
+            }
+
+            return mappedPublishing;
+        }
+
+        public void CreatePublishing(PublishingDTO publishing)
+        {
+            Publishing publishingEntity = mapper.Map<PublishingDTO, Publishing>(publishing);
+            unitOfWork.Publishings.Create(publishingEntity);
+        }
+
+        public void UpdatePublishing(PublishingDTO publishing)
+        {
+            Publishing publishingEntity = mapper.Map<PublishingDTO, Publishing>(publishing);
+            unitOfWork.Publishings.Update(publishingEntity);
+        }
+
+        public void DeletePublishing(int id)
+        {
+            unitOfWork.Publishings.Delete(id);
         }
 
         public IEnumerable<TopicDTO> GetAllTopics()
         {
-            return unitOfWork.Topics.GetAll().Select(x => x.ToDTO());
+            return mapper.Map<IEnumerable<Topic>, IEnumerable<TopicDTO>>(unitOfWork.Topics.GetAll());
         }
     }
 }
---
+++ dotnet-backend/CloudPublishing/Models/Publishings/TopicViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudPublishing.Models.Publishings
+{
+    public class TopicViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
