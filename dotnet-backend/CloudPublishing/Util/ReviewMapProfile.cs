174811557ce65affeb260ad12d1620d115423592
Aleksandr Svetnizki <aleksandr.svetnizki@artezio.com>
2019-03-06 12:23:44 +0300

    Добавлены файлы конфигурации Automapper на PL и BLL. Изменения в контроллере рецензий и в сервисе рецензий - конфигурация automapper заменена на использование профиля.

---
+++ dotnet-backend/CloudPublishing/Util/ReviewMapProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CloudPublishing.Business.DTO;
+using CloudPublishing.Models.Reviews.ViewModels;
+
+namespace CloudPublishing.Util
+{
+    public class ReviewMapProfile : Profile
+    {
+        public ReviewMapProfile()
+        {
+            CreateMap<DetailedReviewDTO, DetailedReviewVM>();
+            CreateMap<ReviewVM, ReviewDTO>();
+            CreateMap<ReviewDTO, ReviewVM>();
+            CreateMap<PublishingDTO, ShortPublishingModel>();
+            CreateMap<TopicDTO, TopicModel>();
+            CreateMap<EmployeeDTO, AuthorModel>();
+            CreateMap<ArticleDTO, ArticleModel>();
+        }
+    }
+}
