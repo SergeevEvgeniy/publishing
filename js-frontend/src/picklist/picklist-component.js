75d8ef087144ee59ffe698bf4b92a9e93b1ee51e
Vadim Putsick <trewyut@mail,ru>
2019-03-11 21:39:26 +0300

    Добавление документации к компоненту PicklistComponent

--- js-frontend/src/picklist/picklist-component.js
+++ js-frontend/src/picklist/picklist-component.js
@@ -1,16 +1,43 @@
-var optionList = require('./picklist.hbs');
+var picklistTemplate = require('./picklist.hbs');
 
+/**
+ * Компонент для отображения выпадающего списка
+ * @param $parentElement jQuery element в котором расплагаеться выпадающий список
+ * @param name Строка присваивается атрибуту name выпадающего списка
+ * @param defaultText Строка представляет собой значение оп умолчанию для выпадающего списка
+ * @constructor
+ */
 function PicklistComponent($parentElement, name, defaultText) {
-    this.render = function render(elementList) {
-        $parentElement.empty().append(optionList({
+    /**
+     * Массив значений выпадающего списка
+     * Каждое значение содержит два параметра
+     * value уникальный идентификатор елемента
+     * text название елемента
+     * @type {Array}
+     */
+    var elementList = [];
+    /**
+     * Задает список элементов
+     * @param list
+     */
+    this.setElementList = function setElementList(list) {
+        elementList = list;
+    };
+    /**
+     *Устанавливает значение выпадающего списка к значению по умолчанию
+     */
+    this.selectDefault = function selectDefault() {
+        $parentElement.find('select').prop('selectedIndex', 0);
+    };
+    /**
+     * Отображает выпадающий список на странице
+     */
+    this.render = function render() {
+        $parentElement.empty().append(picklistTemplate({
             name: name,
             defaultText: defaultText,
             elementList: elementList
         }));
-    };
-
-    this.selectDefault = function selectDefault() {
-        $parentElement.find('select').prop('selectedIndex', 0);
     };
 }
 
