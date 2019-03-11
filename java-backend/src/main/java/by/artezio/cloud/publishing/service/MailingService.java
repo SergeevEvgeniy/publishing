69adcc0d0547fbaec27a9b6e5776bbfe5880beb5
Vladislav Gamezo <Vladislav.Gamezo@artezio.com>
2019-03-11 11:44:00 +0300

    Из сервиса MailingService выделен интерфейс. Сам же MailingService переименован в MailingServiceImpl.

--- java-backend/src/main/java/by/artezio/cloud/publishing/service/MailingService.java
+++ java-backend/src/main/java/by/artezio/cloud/publishing/service/MailingService.java
@@ -1,57 +1,33 @@
 package by.artezio.cloud.publishing.service;
 
-import by.artezio.cloud.publishing.dao.MailingDao;
-import by.artezio.cloud.publishing.dao.PublishingDao;
-import by.artezio.cloud.publishing.dto.MailingInfo;
 import by.artezio.cloud.publishing.domain.Publishing;
-import org.springframework.stereotype.Service;
+import by.artezio.cloud.publishing.dto.MailingInfo;
 
 import java.util.List;
 
 /**
- * Сервис, реализующий логику получения данных для {@link by.artezio.cloud.publishing.web.controllers.MailingController}.
  * @author vgamezo
  */
-@Service
-public class MailingService {
-
-    private MailingDao mailingDao;
-    private PublishingDao publishingDao;
-
-    /**
-     * Конструктор с параметрами.
-     * @param mailingDao mailingDao
-     * @param publishingDao publishingDao
-     */
-    public MailingService(final MailingDao mailingDao, final PublishingDao publishingDao) {
-        this.mailingDao = mailingDao;
-        this.publishingDao = publishingDao;
-    }
+public interface MailingService {
 
     /**
      * Метод, возвращающий список всех рассылок.
      * @return список рассылок
      */
-    public List<MailingInfo> getAllMailingInfo() {
-        return mailingDao.getAllMailingInfo();
-    }
+    List<MailingInfo> getAllMailingInfo();
 
     /**
      * Возвращает список всех публикаций {@link Publishing}, доступных издательству.
      * @return список объектов {@link Publishing}
      */
-    public List<Publishing> getPublishingList() {
-        return publishingDao.getPublishingList();
-    }
+    List<Publishing> getPublishingList();
 
     /**
      * Возвращает список email-адресов, которые принимали участие в последней рассылке публикации с id == {@param id}.
      * @param id id публикации
      * @return Список email-адресов
      */
-    public List<String> getEmailListByPublishingId(final int id) {
-        return mailingDao.getEmailListByPublishingId(id);
-    }
+    List<String> getEmailListByPublishingId(int id);
 
     /**
      * Метод, обновляющий список подписчиков на издание с <code>id == {@param publishingId}</code>.
@@ -59,18 +35,5 @@ public class MailingService {
      * @param emails новый список подписчиков.
      * @return <code>true</code>, если обновление прошло успешно, иначе <code>false</code>.
      */
-    public boolean updateSubscribersListByPublishingId(final int publishingId, final List<String> emails) {
-        boolean wasSuccessUpdated = true;
-
-        Integer mailingId = mailingDao.getMailingIdByPublishingId(publishingId);
-        if (mailingId == null) {
-            mailingId = mailingDao.createNewMailingByPublishingId(publishingId);
-        }
-
-        for (String email : emails) {
-            wasSuccessUpdated &= mailingDao.addNewSubscriberByMailingId(mailingId, email);
-        }
-
-        return wasSuccessUpdated;
-    }
+    boolean updateSubscribersListByPublishingId(int publishingId, List<String> emails);
 }
---
+++ java-backend/src/main/java/by/artezio/cloud/publishing/service/MailingServiceImpl.java
@@ -0,0 +1,61 @@
+package by.artezio.cloud.publishing.service;
+
+import by.artezio.cloud.publishing.dao.MailingDao;
+import by.artezio.cloud.publishing.dao.PublishingDao;
+import by.artezio.cloud.publishing.dto.MailingInfo;
+import by.artezio.cloud.publishing.domain.Publishing;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Сервис, реализующий логику получения данных для {@link by.artezio.cloud.publishing.web.controllers.MailingController}.
+ * @author vgamezo
+ */
+@Service
+public class MailingServiceImpl implements MailingService {
+
+    private MailingDao mailingDao;
+    private PublishingDao publishingDao;
+
+    /**
+     * Конструктор с параметрами.
+     * @param mailingDao mailingDao
+     * @param publishingDao publishingDao
+     */
+    public MailingServiceImpl(final MailingDao mailingDao, final PublishingDao publishingDao) {
+        this.mailingDao = mailingDao;
+        this.publishingDao = publishingDao;
+    }
+
+    @Override
+    public List<MailingInfo> getAllMailingInfo() {
+        return mailingDao.getAllMailingInfo();
+    }
+
+    @Override
+    public List<Publishing> getPublishingList() {
+        return publishingDao.getPublishingList();
+    }
+
+    @Override
+    public List<String> getEmailListByPublishingId(final int id) {
+        return mailingDao.getEmailListByPublishingId(id);
+    }
+
+    @Override
+    public boolean updateSubscribersListByPublishingId(final int publishingId, final List<String> emails) {
+        boolean wasSuccessUpdated = true;
+
+        Integer mailingId = mailingDao.getMailingIdByPublishingId(publishingId);
+        if (mailingId == null) {
+            mailingId = mailingDao.createNewMailingByPublishingId(publishingId);
+        }
+
+        for (String email : emails) {
+            wasSuccessUpdated &= mailingDao.addNewSubscriberByMailingId(mailingId, email);
+        }
+
+        return wasSuccessUpdated;
+    }
+}
