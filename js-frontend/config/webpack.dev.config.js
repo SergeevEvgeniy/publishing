8405cdf56bce3f8421b04d41321eeafdbae26703
ntishkevich <Nikolai.Tishkevich@artezio.com>
2019-03-13 23:14:33 +0300

    New strict rules to prevent a dumb typing

--- js-frontend/config/webpack.dev.config.js
+++ js-frontend/config/webpack.dev.config.js
@@ -3,46 +3,49 @@ var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/app.js',
-  devtool: 'cheap-source-map',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './../dist')
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      },
-      {
-        test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
-        use: [{
-          loader: 'file-loader'
-        }]
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
-      },
-      {
-        test: /\.hbs/,
-        loader: 'handlebars-loader',
-        exclude: /node_modules/
-      }
+    entry: './src/app.js',
+    devtool: 'cheap-source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './../dist')
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: [/node_modules/, /config/],
+                loader: 'eslint-loader',
+                options: {
+                    formatter: require('eslint/lib/formatters/codeframe')
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            {
+                test: /\.hbs/,
+                loader: 'handlebars-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
+                use: [{
+                    loader: 'file-loader'
+                }]
+            },
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        new ExtractTextPlugin('bundle.styles.css')
     ]
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery'
-    }),
-    new ExtractTextPlugin('bundle.styles.css')
-  ]
 };
