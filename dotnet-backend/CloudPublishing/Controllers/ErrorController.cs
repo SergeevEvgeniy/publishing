c2e127ebcf78ab0ac509a03415f9efc967de2ca1
Nikita Podobed <Nikita.Podobed@artezio.com>
2019-03-21 18:24:20 +0300

    Добавлена обработка запросов по несуществующим URL
    - В Web.config добавлена секция httpErrors, в которой настроена обработка ошибки 404
    - Добавлен контроллер ErrorController для обработки ошибки
    - Добавлено представление NotFound.cshtml
    - Добавлен необходимый контент и добавлены стили в файл site.css

---
+++ dotnet-backend/CloudPublishing/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace CloudPublishing.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+
+            return View(404);
+        }
+    }
+}
