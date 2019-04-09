4c5be5552fd24e6efd7c3fcb7d8f516d2276f2aa
Vladislav Gamezo <vladislav.gamezo@artezio.com>
2019-04-09 11:39:15 +0000

    Removed unused imports in LoggingMailSender.java

--- java-backend/src/main/java/by/artezio/cloud/publishing/service/impl/LoggingMailSender.java
+++ java-backend/src/main/java/by/artezio/cloud/publishing/service/impl/LoggingMailSender.java
@@ -4,7 +4,6 @@ import by.artezio.cloud.publishing.domain.MailingResultType;
 import by.artezio.cloud.publishing.service.MailSender;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
